76cd3f00878944f6026e56537552c3563b5fb744
Shaik Sonia <shaiksonia222@gmail.com>
2024-12-26 04:48:29 +0530

    update

--- front-end-programming/ex-4/level-2/my-vite-ts-app/vite.config.ts
+++ front-end-programming/ex-4/level-2/my-vite-ts-app/vite.config.ts
@@ -4,8 +4,8 @@ import eslintPlugin from 'vite-plugin-eslint';
 export default defineConfig({
   plugins: [
     eslintPlugin({
-      fix: true, // Automatically fix linting issues when possible
-      include: ['src/**/*.{ts,tsx,js,jsx}'], // Make sure to lint TypeScript and JavaScript files
+      fix: true,
+      include: ['src/**/*.{ts,tsx,js,jsx}'], 
     }),
   ],
 });
